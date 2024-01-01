2e229809423c8cd8f829a2630e869c204ffb5758
LuanFabricio <luanlimaleite@hotmail.com>
2023-12-31 22:02:06 -0300

    feat: Now the build file compiles the test2.c too

--- examples/01_simple/build.sh
+++ examples/01_simple/build.sh
@@ -1,8 +1,16 @@
 BUILD_FOLDER=build
+OBJ_FOLDER=$BUILD_FOLDER"/objs"
 
 mkdir -p $BUILD_FOLDER
+mkdir -p $OBJ_FOLDER
 
 BINARY_PATH=$BUILD_FOLDER"/main"
 FLAGS="-Wall -Wextra"
 
-gcc $FLAGS -o $BINARY_PATH main.c 
+SHARED_OUTPUT=$OBJ_FOLDER"/test2.so"
+
+gcc -o $SHARED_OUTPUT $FLAGS -fPIC -shared test2.c
+
+if [[ $1 != *"debug"* ]]; then
+	gcc $FLAGS -o $BINARY_PATH main.c
+fi
